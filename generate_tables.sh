f6ffc456219c315be15afb7402f7968614735604
Matthew Greci <mgreci@unm.edu>
2016-09-29 11:31:39 -0600

    Edit generate_tables to work in new structure, move issues.txt into artifacts/

--- generate_tables.sh
+++ generate_tables.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Ensure RAILS_PROJECT is correct
-# Ensure this script is executable
-# Then run this script (./generate_tables.sh)
-
-RAILS_PROJECT=team-manager/
-cd $RAILS_PROJECT
-
+## Scaffolds
 # Person (People) table
 rails g scaffold person id:int first_name:string last_name:string addr_route:string addr_city:string addr_state:string addr_zip:string username:string pass_hash:string pass_salt:string bio:text gender:string height_in_inches:string weight_in_pounds:string birth_month:int birth_day_number:int birth_year:int
 # Phone_number (Phone_numbers) table
